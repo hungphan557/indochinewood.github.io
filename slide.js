84d03007217cb8a5917d7da1f8e1ee2a13620acb
hungphan557 <phanhung12395@gmail.com>
2021-05-29 15:28:50 +0700

    upload

---
+++ slide.js
@@ -0,0 +1,17 @@
+var KichThuoc = document.getElementsByClassName("slide")[0].clientWidth;
+var ChuyenSlide = document.getElementsByClassName("chuyen-slide")[0];
+var Img = ChuyenSlide.getElementsByTagName("img");
+var Max = KichThuoc * Img.length;
+Max -= KichThuoc;
+var Chuyen = 0;
+function Next(){
+	if(Chuyen < Max) Chuyen += KichThuoc;
+	else Chuyen = 0;
+	ChuyenSlide.style.marginLeft = '-' + Chuyen + 'px';
+}
+
+function Back(){
+	if(Chuyen == 0) Chuyen = Max;
+	else Chuyen -= KichThuoc;
+	ChuyenSlide.style.marginLeft = '-' + Chuyen + 'px';
+}
